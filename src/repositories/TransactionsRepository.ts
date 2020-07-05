e20a6a7196ec0ee34d4817eb138230cee13c7c0c
Otavio Ho Aragoni <otavio.aragoni@gmail.com>
2020-07-05 01:31:15 -0300

    Create getBalance method

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,26 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const reducer = (
+      accumulator: Balance,
+      currentValue: Transaction,
+    ): Balance => {
+      if (currentValue.type === 'income') {
+        accumulator.income += currentValue.value;
+        accumulator.total += currentValue.value;
+      } else {
+        accumulator.outcome += currentValue.value;
+        accumulator.total -= currentValue.value;
+      }
+      return accumulator;
+    };
+
+    const allTransactions = await this.find();
+    const initialBalance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    const balance = allTransactions.reduce(reducer, initialBalance);
+
+    return balance;
   }
 }
 
