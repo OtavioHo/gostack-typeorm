c0636ce0a26b622b9119085f2334a0a1bc14da6f
Otavio Ho Aragoni <otavio.aragoni@gmail.com>
2020-07-05 22:42:44 -0300

    Create import service

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,44 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const readCSVStream = fs.createReadStream(filePath);
+    const createTransactionService = new CreateTransactionService();
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const transactions: Transaction[] = [];
+
+    parseCSV.on('data', async row => {
+      const transaction = await createTransactionService.execute({
+        title: row[0],
+        type: row[1],
+        value: parseFloat(row[2]),
+        category: row[3],
+      });
+      transactions.push(transaction);
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    const fileExist = await fs.promises.stat(filePath);
+    if (fileExist) {
+      await fs.promises.unlink(filePath);
+    }
+
+    return transactions;
   }
 }
 
